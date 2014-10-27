5839942f738520e07b648588029dc3eb5cf3ba42
iTidez <iTidez@gmail.com>
2014-10-26 23:52:37 -0400

    Created baseline methods for creation in CartManager

--- src/main/java/com/cobalt/hightidez/carttransport/cart/CartManager.java
+++ src/main/java/com/cobalt/hightidez/carttransport/cart/CartManager.java
@@ -6,6 +6,7 @@ package com.cobalt.hightidez.carttransport.cart;
 
 import java.util.HashMap;
 import java.util.UUID;
+import org.bukkit.Location;
 import org.bukkit.event.Listener;
 
 /**
@@ -20,10 +21,28 @@ public class CartManager implements Listener {
     }
     
     /**
+     * Creates a new TransportCart
+     * @param loc
+     * @return TransportCart
+     */
+    public TransportCart createCart(Location loc) {
+        return new TransportCart(loc);
+    }
+    
+    /**
+     * Returns the TransportCart specified by the Unique ID
+     * @param id
+     * @return TransportCart
+     */
+    public TransportCart getCart(UUID id) {
+        return myRegisteredCarts.containsKey(id) ? myRegisteredCarts.get(id) : null;
+    }
+    
+    /**
      * Registers a new instance of a TransportCart in a static
      * HashMap field for storage purposes
      * @param cart
-     * @return 
+     * @return UUID
      */
     public static UUID registerCart(TransportCart cart) {
         UUID currentId = UUID.nameUUIDFromBytes(cart.toString().getBytes());
@@ -37,7 +56,7 @@ public class CartManager implements Listener {
      * Unregister the TransportCart from existence, uses the cart
      * to call parent method which does the actual erasing
      * @param cart
-     * @return 
+     * @return boolean
      */
     public static boolean unregisterCart(TransportCart cart) {
         return unregisterCart(cart.getId());
@@ -47,7 +66,7 @@ public class CartManager implements Listener {
      * Unregister the TransportCart from existence, uses each
      * carts Unique ID to scan and erase from the HashMap
      * @param id
-     * @return 
+     * @return boolean
      */
     public static boolean unregisterCart(UUID id) {
         if(myRegisteredCarts.containsKey(id)) {
